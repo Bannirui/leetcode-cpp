02fa5fdb601b7ad42acd901101ad216b0d157951
dingrui <Bannirui@outlook.com>
2026-01-09 17:09:40 +0800

    26

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 leetcode-cpp
 ---
+
+actually, for the purpose of studying cpp
+
+- cpp, especial c++11
+- stl, like vector, queue, stack
+- the last is leetcode
---
+++ leetcode/editor/en/[26]Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,97 @@
+//Given an integer array nums sorted in non-decreasing order, remove the 
+//duplicates in-place such that each unique element appears only once. The relative 
+//order of the elements should be kept the same. 
+//
+// Consider the number of unique elements in nums to be k. 
+// After removing duplicates, return the number of unique elements k. 
+//
+// 
+// The first k elements of nums should contain the unique numbers in sorted 
+//order. The remaining elements beyond index k - 1 can be ignored. 
+//
+// Custom Judge: 
+//
+// The judge will test your solution with the following code: 
+//
+// 
+//int[] nums = [...]; // Input array
+//int[] expectedNums = [...]; // The expected answer with correct length
+//
+//int k = removeDuplicates(nums); // Calls your implementation
+//
+//assert k == expectedNums.length;
+//for (int i = 0; i < k; i++) {
+//    assert nums[i] == expectedNums[i];
+//}
+// 
+//
+// If all assertions pass, then your solution will be accepted. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,1,2]
+//Output: 2, nums = [1,2,_]
+//Explanation: Your function should return k = 2, with the first two elements 
+//of nums being 1 and 2 respectively.
+//It does not matter what you leave beyond the returned k (hence they are 
+//underscores).
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,0,1,1,1,2,2,3,3,4]
+//Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+//Explanation: Your function should return k = 5, with the first five elements 
+//of nums being 0, 1, 2, 3, and 4 respectively.
+//It does not matter what you leave beyond the returned k (hence they are 
+//underscores).
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -100 <= nums[i] <= 100 
+// nums is sorted in non-decreasing order. 
+// 
+//
+// Related TopicsArray | Two Pointers 
+//
+// 👍 18341, 👎 20362bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+
+#include "../../../stdc.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        //Input: nums = [0,0,1,1,1,2,2,3,3,4]
+        //Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+        int ret=0; // the next pos for emplacing
+        int cur=0; // [0...cur) are visited
+        int sz=nums.size();
+        while(cur<sz){
+            while(cur>0&&cur<sz&&nums[cur]==nums[cur-1]){
+                // duplicate
+                cur++;
+            }
+            if(cur<sz){
+                nums[ret]=nums[cur];
+                ret++;
+                cur++;
+            }
+        }
+        return ret;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
