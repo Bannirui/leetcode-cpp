8d79c69624cdcd8a5a3940645368e7e249f0db3c
dingrui <Bannirui@outlook.com>
2026-01-09 10:42:29 +0800

    2

---
+++ leetcode/editor/en/[2]Add Two Numbers.cpp
@@ -0,0 +1,89 @@
+//You are given two non-empty linked lists representing two non-negative 
+//integers. The digits are stored in reverse order, and each of their nodes contains a 
+//single digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the 
+//number 0 itself. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have 
+//leading zeros. 
+// 
+//
+// Related TopicsLinked List | Math | Recursion 
+//
+// 👍 35841, 👎 7116bug 反馈 | 使用指南 | 更多配套插件 
+//
+//
+//
+//
+
+
+#include "../../../stdc.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //Input: l1 = [2,4,3], l2 = [5,6,4]
+        //Output: [7,0,8]
+        //Explanation: 342 + 465 = 807.
+        ListNode* head = new ListNode();
+        ListNode* cur=head;
+        int carry=0;
+        while(l1 || l2 || carry) {
+            int v1=l1?l1->val:0;
+            int v2=l2?l2->val:0;
+            int sum=v1+v2+carry;
+            carry=sum/10;
+            cur->next=new ListNode(sum%10);
+            cur=cur->next;
+            l1=l1?l1->next:nullptr;
+            l2=l2?l2->next:nullptr;
+        }
+        cur->next=nullptr;
+        ListNode* ret=head->next;
+        delete head;
+        return ret;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
